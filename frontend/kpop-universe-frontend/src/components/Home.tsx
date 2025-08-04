c1832500c1a0db085e4e11e3cb6ff9a79b56e892
kphitsan <kevinphitsanu22@gmail.com>
2025-08-03 23:39:59 -0400

    Setup mock components, working on routing for Login

---
+++ frontend/kpop-universe-frontend/src/components/Home.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar.tsx"
+export default function Home() {
+    return (
+        <>
+        <Navbar />
+        <h1>This is Home!</h1>
+        </>
+    )
+}
---
+++ frontend/kpop-universe-frontend/src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+export default function() {
+    return (
+        <h1>This is the navbar</h1>
+    )
+}
